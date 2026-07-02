12bc5dbe1726aeb28ad96aa8eedcabd13c7411ce
Ruhul Amin <ruhulamin824854@gmail.com>
2026-07-02 08:06:49 +0600

    Query-2 Complete

--- QUERY.sql
+++ QUERY.sql
@@ -86,3 +86,14 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 -- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
 -- =========================================================================
 select match_id, fixture, base_ticket_price from matches where tournament_category = 'Champions League' and match_status = 'Available';
+
+-- =========================================================================
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+-- =========================================================================
+select user_id, full_name, email from users where full_name like 'Tanvir%' or full_name ilike '%Haque%';
+
+
+
+
+
+
